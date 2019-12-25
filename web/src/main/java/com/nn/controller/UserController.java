aed2dcc68c3a55d79da4355223f6e6acae97c737
HiNN324169 <2026369403@qq.com>
2019-12-25 13:33:59 +0800

    spring + springmvc + mybatis 分模块开发 纯净版

--- web/src/main/java/com/nn/controller/UserController.java
+++ web/src/main/java/com/nn/controller/UserController.java
@@ -1,23 +1,29 @@
 package com.nn.controller;
 
+import com.nn.entity.User;
 import com.nn.service.UserService;
 import com.nn.service.imp.UserServiceImp;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 public class UserController {
+
+    // 日志 输出
+    private static Logger logger = Logger.getLogger(UserController.class);
 
     @Resource(type = UserServiceImp.class)
     private UserService userService;
 
     @RequestMapping("/user")
     public @ResponseBody Object queryAll(){
-
-
-        return userService.queryAll();
+        List<User> users = userService.queryAll();
+        logger.info(users.toString());
+        return users;
     }
 }
